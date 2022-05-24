51a9491dedce1a9bf35f9568106b7ce1f310528c
Tkachuk94 <Tkachuk753@gmail.com>
2022-05-24 00:13:15 -0700

    final push

---
+++ src/components/ContactUs.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Linked in :
+                  <a href="https://www.linkedin.com/in/trent-tkachuk-009b70148/">
+                  {resumeData.linkedinId}
+                  </a>
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/Tkachuk94",
+    "name": "Trent Tkachuk",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/trent-tkachuk-009b70148/",
+    "roleDescription": "I have a passion for learning and building web applications and I am currently looking for a position in the web development field.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/trent-tkachuk-009b70148/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Tkachuk94",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Twitter",
+          "url":"https://twitter.com/tkachuk_trent",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a Full Time Water Utility Supervisor for the city of San Diego, Looking to shift careers into a more technical role. I am looking forward to recieving my certification in Full Stack Web Development from UCSD extension and making the leap into something I love.",
+    "address":"San Diego, CA",
+    "website":"https://github.com/Tkachuk94",
+    "education":[
+      {
+        "UniversityName":"UCSD Extension Web Development Bootcamp",
+        "specialization":"Web Development",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        "Achievements":"Web Development Certification"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"City of San Diego",
+        "specialization":"Water Utility Supervisor",
+        "MonthOfLeaving":"2014 - Present",
+        "YearOfLeaving":"",
+        "Achievements":"Public Service Management certification"
+      },
+
+    ],
+    "skillsDescription":"A list of my skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Programeter",
+        "description":"Our objective was to create an application that would facilitate partnering with other programmers to work on new projects.",
+        "imgurl":"images/portfolio/Programeter.png",
+        "link":'https://glacial-garden-96019.herokuapp.com/'
+      },
+      {
+        "name":"PawPal",
+        "description":"Pawpal is an interactive web application where the user is able to input a desired location within a specified radius to search for adoptable animals. On the homepage, the user is able to view three adoptable animals, a search bar, and a favorites button that leads to a modal that may contain any animals the user may have bookmarked. After the user clicks the search button, the user will be led to a results page containing a list of animals that are adoptable based on the parameters specified in the location and search radius. A filter button containing a variety of attributes will also be available to filter out specific traits the user may want to apply such as gender and age. If the user wants to save a certain pet profile to view for later, they will be able to do so by pressing the paw print located next to each pet's image. The user will be able to easily retrieve the saved pet by clicking the favorites button that is available on the home page and results page.",
+        "imgurl":"images/portfolio/pawpal.png",
+        "link":"https://jmaraya1229.github.io/Pawpal/"
+      },
+      {
+        "name":"Password generator",
+        "description":"a Password Generator that uses a randomly selected mixture of lowerCase letters, upperCase letters, numbers and symbols based upon a given number of characters ranging from 8-128.",  
+        "imgurl":"images/portfolio/passwordGen.png",
+        "link":"https://tkachuk94.github.io/Password-Generator/"
+      },
+      {
+        "name":"Tech Blog",
+        "description":"The purpose of this web application is to allow the user to post blog posts on a blog-style site.",
+        "imgurl":"images/portfolio/TechBlog.png",
+        "link":"https://trentstechblog.herokuapp.com/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Sample Testimonial",
+        "name":"John Doe"
+      },
+      {
+        "description":"Secondary Testimonial",
+        "name":"Jane Doe"
+      }
+    ]
+  }
+  
+  export default resumeData
