597cd4253940311b1c11fdfc67cd5ea69a38914f
Tkachuk94 <Tkachuk753@gmail.com>
2022-05-24 00:12:58 -0700

    Updates

---
+++ precache-manifest.46bb8197c6a309e1b45b6de6c17e3392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6903cc9306f692757ae4",
+    "url": "/React_Portfolio/static/css/main.ba4815e0.chunk.css"
+  },
+  {
+    "revision": "6903cc9306f692757ae4",
+    "url": "/React_Portfolio/static/js/main.6903cc93.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React_Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "3c58e419f48412bd6bf8a1c65f955e1b",
+    "url": "/React_Portfolio/index.html"
+  }
+];
